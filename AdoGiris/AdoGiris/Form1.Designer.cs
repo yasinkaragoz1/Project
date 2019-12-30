45883efd5e7d3b02493f21039b944507771b5635
yasinkaragoz1 <59366969+yasinkaragoz1@users.noreply.github.com>
2019-12-30 19:24:33 +0300

    FirstFolders
    Açıklama

---
+++ AdoGiris/AdoGiris/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace AdoGiris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUnvan = new System.Windows.Forms.ComboBox();
+            this.txtSehir = new System.Windows.Forms.ComboBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnKayit = new System.Windows.Forms.Button();
+            this.txtSehirId = new System.Windows.Forms.TextBox();
+            this.Liste = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unvan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dadi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sehir = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtilceler = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Liste)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUnvan
+            // 
+            this.txtUnvan.FormattingEnabled = true;
+            this.txtUnvan.Items.AddRange(new object[] {
+            "Prof.Dr.",
+            "Doc.Dr.",
+            "Uzman Dr.",
+            "Dr."});
+            this.txtUnvan.Location = new System.Drawing.Point(177, 79);
+            this.txtUnvan.Name = "txtUnvan";
+            this.txtUnvan.Size = new System.Drawing.Size(228, 21);
+            this.txtUnvan.TabIndex = 0;
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtSehir.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.txtSehir.FormattingEnabled = true;
+            this.txtSehir.Location = new System.Drawing.Point(177, 154);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(228, 21);
+            this.txtSehir.TabIndex = 0;
+            this.txtSehir.SelectedIndexChanged += new System.EventHandler(this.txtSehir_SelectedIndexChanged);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(177, 118);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ünvan :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Doktor adı :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Yellow;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 21);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Şehir :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnKayit
+            // 
+            this.btnKayit.Location = new System.Drawing.Point(589, 77);
+            this.btnKayit.Name = "btnKayit";
+            this.btnKayit.Size = new System.Drawing.Size(75, 23);
+            this.btnKayit.TabIndex = 3;
+            this.btnKayit.Text = "Kaydet";
+            this.btnKayit.UseVisualStyleBackColor = true;
+            this.btnKayit.Click += new System.EventHandler(this.btnKayit_Click);
+            // 
+            // txtSehirId
+            // 
+            this.txtSehirId.Location = new System.Drawing.Point(437, 155);
+            this.txtSehirId.Name = "txtSehirId";
+            this.txtSehirId.Size = new System.Drawing.Size(100, 20);
+            this.txtSehirId.TabIndex = 4;
+            // 
+            // Liste
+            // 
+            this.Liste.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Liste.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Unvan,
+            this.Dadi,
+            this.Sehir});
+            this.Liste.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Liste.Location = new System.Drawing.Point(0, 266);
+            this.Liste.Name = "Liste";
+            this.Liste.Size = new System.Drawing.Size(800, 184);
+            this.Liste.TabIndex = 5;
+            this.Liste.DoubleClick += new System.EventHandler(this.Liste_DoubleClick);
+            // 
+            // Id
+            // 
+            this.Id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Width = 41;
+            // 
+            // Unvan
+            // 
+            this.Unvan.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Unvan.HeaderText = "Ünvan";
+            this.Unvan.Name = "Unvan";
+            this.Unvan.Width = 64;
+            // 
+            // Dadi
+            // 
+            this.Dadi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Dadi.HeaderText = "Doktor Adı";
+            this.Dadi.Name = "Dadi";
+            // 
+            // Sehir
+            // 
+            this.Sehir.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Sehir.HeaderText = "Şehir";
+            this.Sehir.Name = "Sehir";
+            this.Sehir.Width = 56;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(589, 117);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 20);
+            this.btnGuncelle.TabIndex = 7;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(589, 155);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 20);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtilceler
+            // 
+            this.txtilceler.FormattingEnabled = true;
+            this.txtilceler.Location = new System.Drawing.Point(177, 190);
+            this.txtilceler.Name = "txtilceler";
+            this.txtilceler.Size = new System.Drawing.Size(228, 21);
+            this.txtilceler.TabIndex = 8;
+            this.txtilceler.SelectedIndexChanged += new System.EventHandler(this.txtilceler_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtilceler);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.Liste);
+            this.Controls.Add(this.txtSehirId);
+            this.Controls.Add(this.btnKayit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.txtUnvan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Liste)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox txtUnvan;
+        private System.Windows.Forms.ComboBox txtSehir;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnKayit;
+        private System.Windows.Forms.TextBox txtSehirId;
+        private System.Windows.Forms.DataGridView Liste;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Unvan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dadi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sehir;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ComboBox txtilceler;
+    }
+}
+
